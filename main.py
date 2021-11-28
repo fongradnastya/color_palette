f00260822a27852abdb6dc29a1f6f8bbe637f2cc
Anastashia Fongrad <fongradnastya@gmail.com>
2021-11-28 14:36:02 +0700

    Add __str__ method

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 class Colour:
-    def __init__(self, colour_code):
+    def __init__(self, colour_code: str):
         self.red = int(colour_code[1:3], 16)
         self.green = int(colour_code[3:5], 16)
         self.blue = int(colour_code[5:7], 16)
-       
+
+    def __str__(self):
+        colour_code = "#{:0>2x}{:0>2x}{:0>2x}".format(
+            self.red, self.green, self.blue)
+        return colour_code.upper()
